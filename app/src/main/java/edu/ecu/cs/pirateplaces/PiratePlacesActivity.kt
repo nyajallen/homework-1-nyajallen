664244d8e18b7e5d43cc7f1953fd8aefcd630648
Mark Hills <mhills@acm.org>
2020-05-15 23:28:39 -0400

    Base code for start of Homework 1

---
+++ app/src/main/java/edu/ecu/cs/pirateplaces/PiratePlacesActivity.kt
@@ -0,0 +1,12 @@
+package edu.ecu.cs.pirateplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PiratePlacesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pirate_places)
+    }
+}
